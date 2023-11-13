bbc5b6144bec1b2a1b0c4adf785ba030a7dbf5a8
suletha <suletha.gopi@gmail.com>
2023-11-13 08:39:51 +0530

    c3

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-import { readFile } from 'node:fs';
+const fs = require('fs');
+const path = require('path');
 
-readFile('./youtube/text.txt', 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname,'youtube','text.txt'), 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 })
+
+console.log("i will be executed first")
 process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err);
   process.exit(1); // Exit the process with a non-zero status code
