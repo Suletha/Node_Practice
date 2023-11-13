575b9367c079c58b6005e6a596631762fd3b92bb
suletha <suletha.gopi@gmail.com>
2023-11-13 08:32:01 +0530

    c2

--- main.mjs
+++ main.mjs
@@ -3,4 +3,8 @@ import { readFile } from 'node:fs';
 readFile('./youtube/text.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
+})
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1); // Exit the process with a non-zero status code
 });
