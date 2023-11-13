4bd3d21f1d7f4203170450e3c6c57e2b608c3f2c
suletha <suletha.gopi@gmail.com>
2023-11-13 09:24:06 +0530

    c9

--- main.js
+++ main.js
@@ -8,7 +8,19 @@ const fileOpe = async () => {
   try{
     const data = await fspromises.readFile(path.join(__dirname,'youtube','text.txt'), 'utf8')
       
-      console.log(data);
+    console.log(data);
+    // //To write into the file
+    await fspromises.writeFile(path.join(__dirname,'youtube','write.txt'), 'welcome to node js')
+    console.log("write operation successful!!!!");
+
+     // To update file
+    await fspromises.appendFile(path.join(__dirname,'youtube','append.txt'), '\n\n welcome to node js')
+    console.log("updated successful!!!!");
+
+    //To rename the file
+    await fspromises.rename(path.join(__dirname,'youtube','append.txt'), path.join(__dirname,'youtube','rename.txt'))
+    console.log("renamed successful!!!!");
+  
     }catch(err){
       console.log(err)
     }
@@ -17,32 +29,3 @@ fileOpe()
 
   
   
-
-
-
-//To read the data from any file
-
-// console.log("i will be executed first")
-
-// //To write into the file
-// fs.writeFile(path.join(__dirname,'youtube','write.txt'), 'welcome to node ja', (err) => {
-//   if (err) throw err;
-//   console.log("write operation successful!!!!");
-//   // To update file
-//   fs.appendFile(path.join(__dirname,'youtube','append.txt'), '\n\n welcome to node ja', (err) => {
-//     if (err) throw err;
-//     console.log("updated successful!!!!");
-//   })
-//   //To rename the file
-//     fs.rename(path.join(__dirname,'youtube','append.txt'), path.join(__dirname,'youtube','rename.txt') , (err) => {
-//       if (err) throw err;
-//       console.log("renamed successful!!!!");
-//     })
-// })
-
-
-
-// process.on('uncaughtException', (err) => {
-//   console.error('Uncaught Exception:', err);
-//   process.exit(1); // Exit the process with a non-zero status code
-// });
